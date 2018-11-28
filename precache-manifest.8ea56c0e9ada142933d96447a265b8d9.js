7aa17b58e36249d98a165e0f0cb5a283fb287850
LV <contact@lavariable.ca>
2018-11-28 06:00:31 -0500

    Updates

---
+++ precache-manifest.8ea56c0e9ada142933d96447a265b8d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/b45baseball1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5277047503202afaa7c4",
+    "url": "/b45baseball1/static/js/runtime~main.52770475.js"
+  },
+  {
+    "revision": "c349a169ecabf91a3ace",
+    "url": "/b45baseball1/static/js/main.c349a169.chunk.js"
+  },
+  {
+    "revision": "68f908c1a87d5bea25dd",
+    "url": "/b45baseball1/static/js/1.68f908c1.chunk.js"
+  },
+  {
+    "revision": "c349a169ecabf91a3ace",
+    "url": "/b45baseball1/static/css/main.72a86420.chunk.css"
+  },
+  {
+    "revision": "675710ec81a4d76b63869a002fd5b6c3",
+    "url": "/b45baseball1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/baseballmk8/precache-manifest.aaf9eb7295d0237dae5582a6b3d403b5.js"
+  "/b45baseball1/precache-manifest.8ea56c0e9ada142933d96447a265b8d9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/baseballmk8/index.html", {
+workbox.routing.registerNavigationRoute("/b45baseball1/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
