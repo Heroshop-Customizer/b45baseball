f16bf5b95df05f29bc5fe5e509c74d6df434ed43
LV <contact@lavariable.ca>
2018-11-28 05:47:31 -0500

    Updates

---
+++ precache-manifest.aaf9eb7295d0237dae5582a6b3d403b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/baseballmk8/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8e39f108f2eff94f938a",
+    "url": "/baseballmk8/static/js/runtime~main.8e39f108.js"
+  },
+  {
+    "revision": "fcbcf75a5026a9544c4a",
+    "url": "/baseballmk8/static/js/main.fcbcf75a.chunk.js"
+  },
+  {
+    "revision": "83ae780d8e60adb818a7",
+    "url": "/baseballmk8/static/js/1.83ae780d.chunk.js"
+  },
+  {
+    "revision": "fcbcf75a5026a9544c4a",
+    "url": "/baseballmk8/static/css/main.4ec6270b.chunk.css"
+  },
+  {
+    "revision": "4332494aef29fdc2d2fd13b936f803d8",
+    "url": "/baseballmk8/index.html"
+  }
+];
